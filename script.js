112ecba699cbae107a7bebd02995981eda0a5eff
stephanypilecarte-collab <stephany.pilecarte@escola.pr.gov.br>
2026-08-25 01:12:13 +0000

    commit 2

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
-let botaoAjuda = document.querySelector(".btn-help");
-let botaoFechar = document.querySelector(".btn-close");
-let janelaDeAjuda = document.querySelector(".modal-fundo");
+// CÓDIGO DO MODAL
+let btnAjuda = document.querySelector(".botao-ajuda");
+let btnFechar = document.querySelector(".botao-fechar");
+let modal = document.querySelector(".modal-fundo");
 
-botaoAjuda.addEventListener("click", abrirModal);
-botaoFechar.addEventListener("click", fecharModal);
+btnAjuda.addEventListener("click", abreModal);
+btnFechar.addEventListener("click", fechaModal);
 
-function abrirModal() {
-  janelaDeAjuda.style.display = "block";
+function abreModal() {
+    modal.style.display = "block";
 }
 
-function fecharModal() {
-  janelaDeAjuda.style.display = "none";
+function fechaModal() {
+    modal.style.display = "none";
+}
+
+
+// TAMANHO DE FONTES
+let tamanhoFonteAtual = 16;
+const valorAdicionado = 2;
+const valorSubtraido = 2;
+
+let btnAumentaFonte = document.getElementById("btnAumentaTexto");
+let btnDiminuiFonte = document.getElementById("btnDiminuiTexto");
+
+btnAumentaFonte.addEventListener("click", aumentaFonte);
+btnDiminuiFonte.addEventListener("click", diminuiFonte);
+
+function aumentaFonte() {
+    tamanhoFonteAtual = tamanhoFonteAtual + valorAdicionado;
+    document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
+}
+
+function diminuiFonte() {
+    tamanhoFonteAtual = tamanhoFonteAtual - valorSubtraido;
+    document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
+}
+
+// LEITURA DE TELA
+
+let lendo = false;
+
+let btnLeitura = document.querySelector(".botao-leitura");
+
+btnLeitura.addEventListener("click", lerEmVozAlta);
+
+function lerEmVozAlta() {
+
+    // se já está lendo
+    if (lendo == true) {
+
+        // se estiver pausado 
+        if (speechSynthesis.paused == true){
+            // continua de onde parou
+            speechSynthesis.resume();
+        } else {
+            // pausa
+            speechSynthesis.pause();
+        }
+        return;
+    }
+
+    let conteudo = document.querySelector("main");
+    let texto = conteudo.innerText;
+
+    let fala = new SpeechSynthesisUtterance(texto);
+
+    fala.lang = "pt-BR";
+    fala.onend = finalizarLeitura;
+
+    lendo = true
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(fala);
+}
+
+function finalizarLeitura() {
+    lendo = false;
 }
